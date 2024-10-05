61e56b248e690f0947ae55e4111321784f682192
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-10-06 01:49:02 +0900

    Update schema to 7.2

--- ruby-on-rails/restful-api/db/schema.rb
+++ ruby-on-rails/restful-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2020_06_26_035204) do
+ActiveRecord::Schema[7.2].define(version: 2020_06_26_035204) do
   create_table "items", charset: "utf8mb4", collation: "utf8mb4_bin", force: :cascade do |t|
     t.string "name"
     t.boolean "done"
