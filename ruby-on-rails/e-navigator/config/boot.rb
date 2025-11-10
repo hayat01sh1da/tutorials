b2fe79ccbdb920f6f549241e315c468143f8b70a
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-11-10 19:09:21 +0900

    Patch malfunctions to run unit tests

--- ruby-on-rails/e-navigator/config/boot.rb
+++ ruby-on-rails/e-navigator/config/boot.rb
@@ -1,3 +1,19 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# Workaround for Ruby 2.6.0 bigdecimal/util.so bug
+# The native extension has undefined symbol: rmpd_util_str_to_d
+# We'll stub it to prevent the crash
+module Kernel
+  alias_method :original_require, :require
+
+  def require(path)
+    if path == 'bigdecimal/util'
+      # bigdecimal/util just adds convenience methods
+      # Skip the broken native extension
+      return true
+    end
+    original_require(path)
+  end
+end
