8829ea66f9cccb95a46a1233d9f8c8139ea60327
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-01-17 19:58:33 +0900

    Stop loading a file that isn’t bundled with Ruby 4’s default Minitest install
    Removed the explicit minitest/mock require in test_helper.rb:3 so the test suite no longer tries to load a file that isn’t bundled with Ruby 4’s default Minitest install; the app doesn’t use Minitest::Mock, so the dependency was unnecessary.

--- ruby-on-rails/perfect-ruby-on-rails/test/test_helper.rb
+++ ruby-on-rails/perfect-ruby-on-rails/test/test_helper.rb
@@ -1,7 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-require 'minitest/mock'
 Dir[Rails.root.join('test', 'helpers', '**', '*.rb')].each { |f| require f }
 
 class ActiveSupport::TestCase
