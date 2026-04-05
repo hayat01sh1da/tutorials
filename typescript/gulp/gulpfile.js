e3bdfa30fa8ed390e56bbef4849f1365a4198abb
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-05 20:13:46 +0900

    Replace browserify with esbuild to resolve elliptic vulnerability (CVE-2025-14505)

--- typescript/gulp/gulpfile.js
+++ typescript/gulp/gulpfile.js
@@ -1,29 +1,16 @@
 const gulp = require("gulp");
-const browserify = require("browserify");
-const source = require("vinyl-source-stream");
-const watchify = require("watchify");
-const tsify = require("tsify");
+const esbuild = require("esbuild");
 const fancy_log = require("fancy-log");
 const paths = { pages: ["./src/*.html"] };
-const watchedBrowserify = watchify(
-  browserify({
-    basedir: ".",
-    debug: true,
-    entries: ["./src/main.ts"],
-    cache: {},
-    packageCache: {},
-  }).plugin(tsify)
-);
 gulp.task("copy-html", () => {
   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
-const bundle = () => {
-  return watchedBrowserify
-    .bundle()
-    .on("error", fancy_log)
-    .pipe(source("bundle.js"))
-    .pipe(gulp.dest("dist"));
+const bundle = async () => {
+  await esbuild.build({
+    entryPoints: ["./src/main.ts"],
+    bundle: true,
+    sourcemap: true,
+    outfile: "./dist/bundle.js",
+  }).catch((err) => fancy_log(err));
 }
 gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
