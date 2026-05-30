ed7d3e74ce32aa294a0aa67446b72d622af4ae5e
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-05-31 01:44:35 +0900

    Fix validation of relation with user model in Model spec

--- ruby-on-rails/restful-api/spec/models/todo_spec.rb
+++ ruby-on-rails/restful-api/spec/models/todo_spec.rb
@@ -6,10 +6,11 @@ require 'rails_helper'
 # Test suite for the Todo model
 RSpec.describe Todo do
   # Association test
+  # ensure Todo model belongs to the User model
+  it { is_expected.to belong_to(:user) }
   # ensure Todo model has a 1:m relationship with the Item model
   it { is_expected.to have_many(:items).dependent(:destroy) }
   # Validation tests
   # ensure columns title and user_id are present before saving
   it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:user_id) }
 end
