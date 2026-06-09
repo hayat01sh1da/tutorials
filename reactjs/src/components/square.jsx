75d6b72624d216d21b358763c97e0a3dfea76e92
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-06-09 10:19:34 +0900

    Migrate from JS to JSX

--- reactjs/src/components/square.jsx
+++ reactjs/src/components/square.jsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 const Square = (props) => {
   return (
     <button className="square" onClick={props.onClick}>{props.value}</button>
